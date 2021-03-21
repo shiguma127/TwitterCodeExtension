5cf0ced69f8694b8c2d9f23b4dc1a612540ccdb2
tomotomo <tomoppeko@gmail.com>
2021-03-21 17:09:43 +0900

    fix: issue where filenames will be empty

--- TwitterCodeExtension/options.js
+++ TwitterCodeExtension/options.js
@@ -1,32 +1,32 @@
 hljs.highlightAll();
 
-const filenames = [1, 2];
 chrome.runtime.getPackageDirectoryEntry(function (directoryEntry) {
-    directoryEntry.getDirectory('highlight', {}, function (highlightDir) {
-        highlightDir.getDirectory('styles', {}, function (stylesDir) {
-            const directoryReader = stylesDir.createReader();
-            directoryReader.readEntries(function (entries) {
-                for (entry of entries) {
-                    if (entry.name.match(/.*.css$/))
-                        filenames.push(entry.name);
+    directoryEntry.getDirectory('highlight/styles', {}, function (stylesDir) {
+        const directoryReader = stylesDir.createReader();
+        const filenames = [];
+        (function readNext() {
+            directoryReader.readEntries(function(entries) {
+                if (entries.length) {
+                    for (const entry of entries) {
+                        if (entry.name.match(/.*.css$/))
+                            filenames.push(entry.name);
+                    }
+                    readNext();
+                } else {
+                    for (file of filenames) {
+                        console.log(file)
+                    }
+
+                    const selector = document.getElementById('selector');
+                    for (filename of filenames) {
+                        var option = document.createElement("option");
+                        option.text = filename;
+                        option.value = filename;
+                        selector.appendChild(option);
+                    }
                 }
-            })
-        })
-    });
+            });
+        })();
+    })
 })
-
-setTimeout(function () {
-    console.log(filenames)
-    for (file of filenames) {
-        console.log(file)
-    }
-}, 100)
-
-const selector = document.getElementById('selector');
-for (filename of filenames) {
-    var option = document.createElement("option");
-    option.text = filename;
-    option.value = filename;
-    selector.appendChild(option);
-}
 //reference https://stackoverflow.com/questions/43008952/how-to-access-a-specific-folder-to-get-file-names-within-my-extension
